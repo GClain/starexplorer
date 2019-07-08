ee76e44a63c89f228ee44450a1298fd01e8b97a3
Linghz <gc.webmaster69@gmail.com>
2019-07-08 21:32:05 +0200

    mise en place ui text

--- main.lua
+++ main.lua
@@ -81,4 +81,18 @@ ship = display.newImageRect(mainGroup, objecSheet, 4, 98, 79)
 ship.x = display.contentCenterX
 ship.y = display.contentHeight -100
 physics.addBody(ship, { radius = 30, isSensor= true})
+-- sensor = detect collision mais sans reponse physique (genre de trigger dans unity)
 ship.myName ="ship"
+
+
+-- Display lives and score
+livesText = display.newText(uiGroup, "Lives: ".. lives, 200, 80, native.systemFont, 36)
+scoreText = display.newText(uiGroup, "Score: ".. score, 400, 80, native.systemFont, 36)
+
+-- Hide the status bar
+display.setStatusBar (display.HiddenStatusBar)
+
+local function updateText()
+    livesText.text = "Lives: ".. lives
+    scoreText.text = "Score: ".. lives
+end
