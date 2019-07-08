e384f0b6595fed9c83f41523c64fc6360496c424
Linghz <gc.webmaster69@gmail.com>
2019-07-09 00:08:10 +0200

    astreroid properties

--- main.lua
+++ main.lua
@@ -96,3 +96,32 @@ local function updateText()
     livesText.text = "Lives: ".. lives
     scoreText.text = "Score: ".. lives
 end
+
+local function createAteroid()
+    local newAsteroid = display.newImageRect(mainGroup, objecSheet, 1, 102, 85)
+    table.insert (asteroidTable, newAsteroid)
+    physics.addBody(newAsteroid, "dynamic", { radius=40, bounce=0.8 })
+    newAsteroid.myName ="asteroid"
+
+    local whereFrom = math.random (3)
+    if (whereFrom == 1) then
+        -- from left
+        newAsteroid.x = -60
+        newAsteroid.y = math.random (500)
+        newAsteroid:setLinearVelocity(math.random (40,120), math.random (20,60))
+
+    elseif (whereFrom == 2) then    
+        -- from top
+        newAsteroid.x = math.random(display.contentWidth)
+        newAsteroid.y = -60
+        newAsteroid:setLinearVelocity(math.random (40,40), math.random (40,120))
+
+    elseif (whereFrom == 3) then    
+        -- from right
+        newAsteroid.x = display.contentWidth + 60
+        newAsteroid.y = math.random (500)
+        newAsteroid:setLinearVelocity(math.random (-120,-40), math.random (20,60))
+    end
+
+    newAsteroid:applyTorque (math.random(-6,6))
+end
